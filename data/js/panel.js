92f662c5ea3ffd920d55db1909bddfbaa8b2e1d4
Aurélien MANCA <aurelien.manca@gmail.com>
2013-07-22 18:38:19 +0200

    Version 0.3 : added support for input[type=range], made the widget icon automatically resized according to the timer

--- data/js/panel.js
+++ data/js/panel.js
@@ -20,19 +20,57 @@ var et = {
     });
 
     self.port.on('update-countdown', function (time) {
-      document.getElementById('countdown').innerHTML = time;
+      var cd = document.getElementById('countdown');
+      while (cd.firstChild) {
+        cd.removeChild(cd.firstChild);
+      }
+      cd.appendChild(document.createTextNode(time));
+
+      if (time.length < 1) {
+        var ca = document.getElementById('cancel');
+        while (ca.firstChild) {
+          ca.removeChild(ca.firstChild);
+        }
+        ca.appendChild(document.createTextNode('Cancel'));
+        document.getElementById('submit').disabled = false;
+        document.getElementById('cancel').disabled = true;
+      }
     });
   },
 
   listenLocal : function () {
+    document.getElementById('timer-hour').addEventListener('input', this.inputRangeListener, false);
+    document.getElementById('timer-minute').addEventListener('input', this.inputRangeListener, false);
+    document.getElementById('timer-second').addEventListener('input', this.inputRangeListener, false);
     document.getElementById('egg-timer-form').addEventListener('submit', this.timerFormListener, false);
     document.getElementById('cancel').addEventListener('click', this.cancelTimerListener, false);
   },
 
+  inputRangeListener : function (event) {
+    var va = document.getElementById(event.target.id + '-value');
+    while (va.firstChild) {
+      va.removeChild(va.firstChild);
+    }
+    va.appendChild(document.createTextNode(event.target.value));
+
+    if (event.target.value > 1) {
+      document.getElementById(event.target.id + '-label-plural').classList.remove('hidden');
+    }
+    else {
+      document.getElementById(event.target.id + '-label-plural').classList.add('hidden');
+    }
+  },
+
   cancelTimerListener : function (event) {
     event.preventDefault();
+
     self.port.emit('cancel');
-    document.getElementById('cancel').innerHTML = 'Cancel';
+
+    var ca = document.getElementById('cancel');
+    while (ca.firstChild) {
+      ca.removeChild(ca.firstChild);
+    }
+    ca.appendChild(document.createTextNode('Cancel'));
     document.getElementById('submit').disabled = false;
     document.getElementById('cancel').disabled = true;
   },
@@ -62,7 +100,12 @@ var et = {
         'timeHumanized' : { h: h, m: m, s: s },
         'notification' : (n === undefined ? 1 : n)
       });
-      document.getElementById('cancel').innerHTML = 'Stop';
+
+      var ca = document.getElementById('cancel');
+      while (ca.firstChild) {
+        ca.removeChild(ca.firstChild);
+      }
+      ca.appendChild(document.createTextNode('Stop'));
       document.getElementById('cancel').disabled = false;
       document.getElementById('submit').disabled = true;
     }
